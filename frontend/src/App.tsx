3bc4f40e52f5a99b54e936cd251ba4b1e2f364d6
WUTONK <wjt474696120@gmail.com>
2025-07-18 06:16:11 +0800

    完成登陆页面逻辑

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Api } from './shared'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("")
+  const [token, setToken] = useState("")
 
   return (
     <>
+     <div className='app-container'>
+      <div className='input-password'>
+        <input
+          type="text"
+          value={username}
+          placeholder="请输入用户名"
+          onChange={(e) => setUsername(e.target.value)}
+          className="username-input"
+        />
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+            type="text"
+            value={password}
+            placeholder="请输入用户名"
+            onChange={(e) => setPassword(e.target.value)}
+            className="password-input"
+          />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+      className='login-button'
+      title="登陆"
+      onClick={()=>{
+        Api.userLoginPost(
+          {
+            loginReq:{
+              username,
+              password
+            } 
+          }
+        ).then((res)=>{
+          setToken(res.token)
+          alert(token)
+        }).catch((err) => {alert(err.message)})
+      }}
+      />
+     </div>
     </>
   )
 }
